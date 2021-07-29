ad50bd14b54652ffbca32e4c13cbb210861a47e5
akkyflair <akkyflair@gmail.com>
2021-07-29 19:46:52 +0900

    ユーザー名を表示しようまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,13 +41,14 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user = User.find_by(password: params[:password])
+    @user = User.find_by(name: params[:name],password: params[:password])
     if @user
       session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
-      @error_message = "パスワードが間違っています"
+      @error_message = "名前またはパスワードが間違っています"
+      @name = params[:name]
       @password = params[:password]
       render("users/login_form")
     end
