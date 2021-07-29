e2e19d99b9e8f538d295bb66d3b895416c74843e
akkyflair <akkyflair@gmail.com>
2021-07-29 19:07:59 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_044558) do
+ActiveRecord::Schema.define(version: 2021_07_28_063958) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_044558) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
