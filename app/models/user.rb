266841894d21f494a36f1c0b74a07998f25493bc
akkyflair <akkyflair@gmail.com>
2021-07-30 01:37:40 +0900

    詳細に投稿を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :name, {presence: true, uniqueness: true}
   validates :password, {presence: true, uniqueness: true}
+
+  def posts
+    return Post.where(user_id: self.id)
+  end
+  
 end
