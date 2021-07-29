69fef1eebc62f8b4c4451adf8d7e8451723b1d52
akkyflair <akkyflair@gmail.com>
2021-07-30 01:46:02 +0900

    投稿者だけが編集できるように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :authenticate_user
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -47,6 +48,14 @@ class PostsController < ApplicationController
     @post.destroy
     flash[:notice] = "マインドを削除しました"
     redirect_to("/posts/index")
+  end
+  
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
   
 end
