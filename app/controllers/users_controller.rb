ed722ee76807203afa8d6ea392ffd063cd5f6472
akkyflair <akkyflair@gmail.com>
2021-07-29 19:25:28 +0900

    修正3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(name: params[:name])
+    @user = User.new(name: params[:name], password: params[:password])
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -39,4 +40,24 @@ class UsersController < ApplicationController
   def login_form
   end
 
+  def login
+    @user = User.find_by(password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message = "パスワードが間違っています"
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+  
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+  
 end
+
