f66c5b7fdb2c6590a3e39eab1d6961485535fb86
akkyflair <akkyflair@gmail.com>
2021-08-01 19:36:06 +0900

    ルーティング書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,40 @@
 Rails.application.routes.draw do
-  post "mindcatches/:post_id/create" => "mindcatches#create"
-  post "mindcatches/:post_id/destroy" => "mindcatches#destroy"
-  
-  
-  post "users/:id/update" => "users#update"
-  get "users/:id/edit" => "users#edit"
-  post "users/create" => "users#create"
+  get "/" => "home#top"
+  get "about" => "home#about"
   get "signup" => "users#new"
-  get "users/index" => "users#index"
-  get "users/:id" => "users#show"
-
   post "login" => "users#login"
   post "logout" => "users#logout"
   get "login" => "users#login_form"
+
+  resources :posts do
+    resources :users do
+      resources :mindcatches, only: [:create, :destoroy]
+    end
+  end
+
+  # post "mindcatches/:post_id/create" => "mindcatches#create"
+  # post "mindcatches/:post_id/destroy" => "mindcatches#destroy"
   
-  get "posts/index" => "posts#index"
-  get "posts/new" => "posts#new"
-  get "posts/:id" => "posts#show"
-  post "posts/create" => "posts#create"
-  get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/update" => "posts#update"
-  post "posts/:id/destroy" => "posts#destroy"
+  # post "users/:id/update" => "users#update"
+  # get "users/:id/edit" => "users#edit"
+  # post "users/create" => "users#create"
+  # get "users/index" => "users#index"
+  # get "users/:id" => "users#show"
+
+  # get "signup" => "users#new"
+
+  # post "login" => "users#login"
+  # post "logout" => "users#logout"
+  # get "login" => "users#login_form"
   
-  get "/" => "home#top"
-  get "about" => "home#about"
+  # get "posts/index" => "posts#index"
+  # get "posts/new" => "posts#new"
+  # get "posts/:id" => "posts#show"
+  # post "posts/create" => "posts#create"
+  # get "posts/:id/edit" => "posts#edit"
+  # post "posts/:id/update" => "posts#update"
+  # post "posts/:id/destroy" => "posts#destroy"
+
+  # get "/" => "home#top"
+  # get "about" => "home#about"
 end
