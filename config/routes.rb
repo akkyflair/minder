589542198d4ef6c82e6ca2bb0b62ad3ee7ecc074
akkyflair <akkyflair@gmail.com>
2021-08-03 03:40:03 +0900

    日付など　修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,47 @@
 Rails.application.routes.draw do
-  get "/" => "home#top"
-  get "about" => "home#about"
-  get "signup" => "users#new"
-  post "login" => "users#login"
-  post "logout" => "users#logout"
-  get "login" => "users#login_form"
   
-  resources :users 
-  resources :mindcatches, only: [:create, :destoroy] do
-    resources :posts 
-  end
-  resources :posts 
-  
+  # get "/" => "home#top"
 
-  # post "mindcatches/:post_id/create" => "mindcatches#create"
-  # post "mindcatches/:post_id/destroy" => "mindcatches#destroy"
-  
-  # post "users/:id/update" => "users#update"
-  # get "users/:id/edit" => "users#edit"
-  # post "users/create" => "users#create"
-  # get "users/index" => "users#index"
-  # get "users/:id" => "users#show"
+  # get "about" => "home#about"
 
   # get "signup" => "users#new"
 
   # post "login" => "users#login"
+
   # post "logout" => "users#logout"
+
   # get "login" => "users#login_form"
   
-  # get "posts/index" => "posts#index"
-  # get "posts/new" => "posts#new"
-  # get "posts/:id" => "posts#show"
-  # post "posts/create" => "posts#create"
-  # get "posts/:id/edit" => "posts#edit"
-  # post "posts/:id/update" => "posts#update"
-  # post "posts/:id/destroy" => "posts#destroy"
+  # resources :users 
+  # resources :mindcatches, only: [:create, :destoroy] do
+  #   resources :posts 
+  # end
+  # resources :posts 
+  
 
-  # get "/" => "home#top"
-  # get "about" => "home#about"
+  post "mindcatches/:post_id/create" => "mindcatches#create"
+  post "mindcatches/:post_id/destroy" => "mindcatches#destroy"
+  
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
+  get "signup" => "users#new"
+
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  get "login" => "users#login_form"
+  
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+
+  get "/" => "home#top"
+  get "about" => "home#about"
 end
